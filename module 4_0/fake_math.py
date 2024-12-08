e8c0ece13627d07cd094f8b40b9c7048b6053945
SelAlexander <122175416+SelAlexander@users.noreply.github.com>
2024-12-08 15:00:45 +0300

    Add files via upload

---
+++ module 4_0/fake_math.py
@@ -0,0 +1,8 @@
+
+def divide (first, second) :
+    if second != 0:
+        print(f"{first} : {second} = {first // second}")
+
+    elif second == 0:
+        print("Ошибка")
+
---
+++ module 4_0/true_math.py
@@ -0,0 +1,10 @@
+import math
+from math import inf
+def divide (first, second) :
+    if second != 0:
+        print(f"{first} : {second} = {first // second}")
+
+    elif second == 0:
+        print(math.inf)
+        #print("∞")
+
